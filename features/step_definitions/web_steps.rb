5bd511c1f1ba99b4f52ea9f6476aa25b0afe3595
DaReal Nabeel <nabeelsaleem@berkeley.edu>
2016-03-09 08:29:39 +0000

    Non-admin bass

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -61,6 +61,18 @@ And /^I am logged into the admin panel$/ do
   end
 end
 
+And /^I am logged in as a user$/ do
+  visit '/accounts/login'
+  fill_in 'user_login', :with => 'nabeel'
+  fill_in 'user_password', :with => 'aguwaar'
+  click_button 'Login'
+  if page.respond_to? :should
+    page.should have_content('Login successful')
+  else
+    assert page.has_content?('Login successful')
+  end
+end
+
 # Single-line step scoper
 When /^(.*) within (.*[^:])$/ do |step, parent|
   with_scope(parent) { When step }
